e38a70df870fdd83e1186dc50c201100a509bd83
Oscar Romanini <oscar-romanini@hotmail.com>
2019-07-31 00:28:58 -0300

    do pre processing

--- meat-app-starter-master/src/app/app.module.ts
+++ meat-app-starter-master/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import {ROUTES} from 'app.routes'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -39,7 +39,7 @@ import { RatingComponent } from './shared/rating/rating.component';
     HttpModule,
     FormsModule,
     SharedModule.forRoot(), //Shared module + providers (Core se torna obsoleto)
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules}) //pré carregamento dos módulos
   ],
   providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
